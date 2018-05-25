23a28b6f8e956e4634dfc5ac2b051ea81d72c892
ranferi <ranferi.velo@gmail.com>
2018-05-24 20:48:19 -0500

    Se agrego unos metodos para insertar y ver usuarios en onto

--- public/index.php
+++ public/index.php
@@ -107,14 +107,74 @@ $app->get('/messages/{id}', function (Request $request, Response $response) {
     $db = new DbOperation();
     $messages = $db->getMessages($userid);
 
+    $response->getBody()->write(json_encode(array("messages" => $messages)));
 
     // $foaf = new EasyRdf_Graph("http://njh.me/foaf.rdf");
     // $foaf->load();
     // $me = $foaf->primaryTopic();
     // echo "My name is: ".$me->get('foaf:name')."\n";
 
-    $response->getBody()->write(json_encode(array("messages" => $messages)));
     // $response->getBody()->write("My name is: " . $me->get('foaf:name')."\n");
+});
+
+// obtener todos las triples que tengan como predicado su:email
+$app->get('/emailAsPredicate', function (Request $request, Response $response) {
+    // $gs = new EasyRdf_GraphStore('http://localhost:3030/susibo/data/');
+    EasyRdf_Namespace::set('su', 'http://www.semanticweb.org/vlim1/ontologies/2018/4/susibo#');
+    $sparql = new EasyRdf_Sparql_Client('http://localhost:3030/susibo/sparql');
+    require_once "html_tag_helpers.php";
+    $result = $sparql->query(
+        'SELECT ?subject ?predicate ?object ' .
+        ' WHERE {'.
+        '  ?subject su:email ?object .'.
+        '}'
+    );
+    foreach ($result as $row) {
+        echo "<li>".link_to($row->subject, $row->object)."</li>\n";
+    }
+});
+
+
+// insertar un usuario directo
+$app->post('/insertUser', function (Request $request, Response $response) {
+    EasyRdf_Namespace::set('su', 'http://www.semanticweb.org/vlim1/ontologies/2018/4/susibo#');
+    $gs = new EasyRdf_GraphStore('http://localhost:3030/susibo/data');
+    // Add the current time in a graph
+    $graph1 = new EasyRdf_Graph();
+    $graph1->add('su:i0434', 'su:nombre', 'Craig');
+    $graph1->add('su:i0434', 'su:apellidoPaterno', 'Ellis');
+    $graph1->add('su:i0434', 'su:email', 'craigellis@yahoo.com');
+    $gs->insertIntoDefault($graph1);
+    // Get the graph back out of the graph store and display it
+    // $graph2 = $gs->get('time.rdf');
+    //print $graph2->dump();
+
+    if (isTheseParametersAvailable(array('name', 'email', 'password', 'gender'))) {
+        $id = $request->getAttribute('id');
+
+        $requestData = $request->getParsedBody();
+
+        $name = $requestData['name'];
+        $email = $requestData['email'];
+        $password = $requestData['password'];
+        $gender = $requestData['gender'];
+
+
+        $db = new DbOperation();
+
+        $responseData = array();
+
+        if ($db->updateProfile($id, $name, $email, $password, $gender)) {
+            $responseData['error'] = false;
+            $responseData['message'] = 'Actualización exitosa';
+            $responseData['user'] = $db->getUserByEmail($email);
+        } else {
+            $responseData['error'] = true;
+            $responseData['message'] = 'No se actualizó';
+        }
+
+        $response->getBody()->write(json_encode($responseData));
+    }
 });
 
 //updating a user
@@ -191,7 +251,7 @@ function isTheseParametersAvailable($required_fields)
         $response = array();
         $response["error"] = true;
         $response["message"] = 'Los campos ' . substr($error_fields, 0, -2) . ' faltan o están vacíos';
-        echo json_encode($response);
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
         return false;
     }
     return true;
