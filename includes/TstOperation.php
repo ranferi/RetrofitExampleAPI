5d0d947b52bb0663501d742ef20deb03b0eed5dc
Ranferi <ranferi.velo@gmail.com>
2019-01-11 20:42:06 -0600

    Se modifico register para tener campos opcionales

--- includes/TstOperation.php
+++ includes/TstOperation.php
@@ -12,6 +12,8 @@ class TstOperation
 
         EasyRdf_Namespace::set('su', 'http://www.semanticweb.org/ranferi/ontologies/2018/9/ssrsi_onto#');
         EasyRdf_Namespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+        EasyRdf_Namespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+        EasyRdf_Namespace::set('owl', 'http://www.w3.org/2002/07/owl#');
 
         $this->gs = new EasyRdf_GraphStore('http://localhost:3030/ssrsi/data');
 
@@ -313,7 +315,7 @@ class TstOperation
         }"
         );
 
-        //print_r($result);
+        print_r($result);
 
         $users = array();
 
@@ -326,5 +328,147 @@ class TstOperation
             array_push($users, $temp);
         }
         return $users;
+    }
+
+    function getAllPoints()
+    {
+        $result = $this->endpoint->query("
+        SELECT ?sujeto ?id ?medi ?latitud ?longitud ?dir ?musica
+        WHERE {
+            ?sujeto su:idSitio ?id .
+            ?sujeto su:tieneUnValorMEDI ?medi .
+            ?sujeto su:tienePropiedad ?prop .
+            ?prop su:latitud ?latitud .
+            ?sujeto su:tienePropiedad ?prop1 .
+            ?prop1 su:longitud ?longitud .
+            ?sujeto su:tienePropiedad ?prop2 .
+            ?prop2 su:direccionSitio ?dir .
+            ?sujeto su:tienePropiedad ?prop3 .
+            ?prop3 su:musica ?musica .
+        }"
+        );
+
+        //print_r($result);
+
+        $points = array();
+
+        foreach ($result as $place) {
+            $temp = array();
+            $temp_id = $place->id->getValue();
+            $temp['id'] = $temp_id;
+            $temp['medi'] = $place->medi->localName();
+            $temp['latitud'] = $place->latitud->getValue();
+            $temp['longitud'] = $place->longitud->getValue();
+            $temp['direccion'] = $place->dir->getValue();
+            $temp['musica'] = $place->musica->getValue();
+            // array_push($points, $temp);
+
+            $second = $this->endpoint->query("
+                SELECT ?sujeto ?nombreSitio ?base
+                WHERE {
+                    ?sujeto su:idSitio " . $temp_id . " .
+                    ?sujeto su:tienePropiedad ?prop .
+                    ?prop su:nombreSitio ?nombreSitio .
+                    ?prop su:provieneDeBD ?base .
+                }"
+            );
+            // print_r($second);
+
+            $names = array();
+            foreach ($second as $name) {
+                $temp_2 = array();
+                $temp_2['nombre_sitio'] = $name->nombreSitio->getValue();
+                $temp_2['proviene'] = $name->base->localName();
+                array_push($names, $temp_2);
+            }
+            $temp['nombres'] = $names;
+
+            $third = $this->endpoint->query("
+                SELECT ?sujeto ?calificacion ?base
+                WHERE {
+                    ?sujeto su:idSitio " . $temp_id . " .
+                    ?sujeto su:tienePropiedad ?prop .
+                    ?prop su:calificacion ?calificacion .
+                    ?prop su:provieneDeBD ?base .
+                }"
+            );
+
+            $ratings = array();
+            foreach ($third as $rating) {
+                $temp_3 = array();
+                $temp_3['calificacion'] = $rating->calificacion->getValue();
+                $temp_3['proviene'] = $rating->base->localName();
+                array_push($ratings, $temp_3);
+            }
+            $temp['calificaciones'] = $ratings;
+
+
+            $fourth = $this->endpoint->query("
+                SELECT ?categoria ?superclase
+                WHERE {
+                    ?s su:idSitio " . $temp_id . " .
+                    ?s su:tienePropiedad ?prop .
+                    ?prop su:categoria ?prop1 .
+                    ?prop1 rdf:type ?categoria .
+                    ?categoria rdfs:subClassOf [ rdf:rest* [ owl:onProperty su:esParteDeBD ; owl:allValuesFrom ?superclase ] ]
+                    FILTER NOT EXISTS { 
+                        ?prop1 a ?otra .
+                        ?otra rdfs:subClassOf ?categoria .
+                        FILTER(?otra != ?categoria)
+                    }
+                }"
+            );
+
+            $cats = array();
+            foreach ($fourth as $cat) {
+                $temp_3 = array();
+                $temp_3['categoria'] = $cat->categoria->localName();
+                $temp_3['proviene'] = $cat->superclase->localName();
+                array_push($cats, $temp_3);
+            }
+            $temp['categorias'] = $cats;
+
+
+            $fifth = $this->endpoint->query("
+                SELECT ?sujeto ?imagen
+                WHERE {
+                    ?sujeto su:idSitio " . $temp_id . " .
+                    ?sujeto su:tienePropiedad ?prop .
+                    ?prop su:imagenSitio ?calificacion .
+                }"
+            );
+
+            $images = array();
+            foreach ($fifth as $img) {
+                $temp_3 = array();
+                $temp_3['imagen'] = $img->imagen->getValue();
+                array_push($images, $temp_3);
+            }
+            $temp['imagenes'] = $images;
+
+
+            $sixth = $this->endpoint->query("
+                SELECT ?sujeto ?comentario ?base
+                WHERE {
+                    ?sujeto su:idSitio " . $temp_id . " .
+                    ?sujeto su:tieneComentario ?prop .
+                    ?prop su:comentario ?comentario .
+                    ?prop su:provieneDeBD ?base .
+                }"
+            );
+
+            $comments = array();
+            foreach ($sixth as $comment) {
+                $temp_3 = array();
+                $temp_3['comentario'] = $comment->comentario->getValue();
+                $temp_3['base'] = $comment->base->localName();
+                array_push($comments, $temp_3);
+            }
+            $temp['comentarios'] = $comments;
+
+
+            array_push($points, $temp);
+        }
+        return $points;
     }
 }
--- public/index.php
+++ public/index.php
@@ -30,8 +30,21 @@ require __DIR__ . '/../src/routes.php';
 $app->run();*/
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
+use NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer;
 // use \Psr\Http\Message\ResponseInterface as Response;
 use Slim\Http\Response;
+
+use \NlpTools\Tokenizers\WhitespaceTokenizer;
+use \NlpTools\Similarity\JaccardIndex;
+use \NlpTools\Similarity\CosineSimilarity;
+use \NlpTools\Similarity\Simhash;
+
+use NlpTools\Models\FeatureBasedNB;
+use NlpTools\Documents\TrainingSet;
+use NlpTools\Documents\TokensDocument;
+use NlpTools\FeatureFactories\DataAsFeatures;
+use NlpTools\Classifiers\MultinomialNBClassifier;
+
 
 require '../vendor/autoload.php';
 require_once '../includes/TstOperation.php';
@@ -119,6 +132,148 @@ $app->get('/users', function (Request $request, Response $response) {
     $tst = new TstOperation();
     $users = $tst->getAllUsers();
     return $response->withJson(array("users" => $users));
+});
+
+
+$app->get('/prueba', function (Request $request, Response $response) {
+    $tst = new TstOperation();
+    $places = $tst->getAllPoints();
+    return $response->withJson(array("places" => $places));
+});
+
+/**
+ * Se obtienen todos lo usuarios
+ */
+$app->get('/nlp', function (Request $request, Response $response) {
+    /*$s1 = "La comida no está tan buena, abusan de la grasa. No es malo el lugar pero por los precios podrías esperar más.
+";
+    $s2 = "por los precios podrías esperar más";
+
+    $tok = new WhitespaceTokenizer();
+    $J = new JaccardIndex();
+    $cos = new CosineSimilarity();
+    $simhash = new Simhash(16); // 16 bits hash
+
+    $setA = $tok->tokenize($s1);
+    $setB = $tok->tokenize($s2);
+
+    printf (
+        "
+    Jaccard:  %.3f
+    Cosine:   %.3f
+    Simhash:  %.3f
+    SimhashA: %s
+    SimhashB: %s
+    ",
+        $J->similarity(
+            $setA,
+            $setB
+        ),
+        $cos->similarity(
+            $setA,
+            $setB
+        ),
+        $simhash->similarity(
+            $setA,
+            $setB
+        ),
+        $simhash->simhash($setA),
+        $simhash->simhash($setB)
+    );*/
+
+
+    // ---------- Data ----------------
+// data is taken from http://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
+// we use a part for training
+    $testing = array(
+        array('Barato','Precios accesibles'),
+    array('Barato','A muy buen precio'),
+    array('Moderado','A buen precio'),
+    array('Moderado','Buena relación precio-costo'),
+    array('Moderado','Precios no tan caros'),
+    array('Moderado','Precio muy bueno'),
+    array('Moderado','Los precios son justos para la calidad'),
+    array('Moderado','Precio bueno a pesar de'),
+    array('Moderado','Precio bueno para ser'),
+    array('Caro','por los precios podrías esperar más'),
+    array('Caro','Un poco caro'),
+    array('Caro','Precios un poco elevados'),
+    array('Caro','En general caro'),
+    array('Caro','Precios algo caros'),
+    array('Caro','Comida cara para la ración que sirven'),
+    array('Caro','Precios excesivos'),
+    array('Muy caro','Caro y malo')
+    );
+
+
+// and another for evaluating
+$training = array(
+    array('Muy caro','Caro y malo, el alambre tenía pura cebolla y se tardaron horas en atender.'),
+    array('Moderado','Tiene buena comida, buena cantina y buen ambiente, con música de salterio. La atención es muy buena. El lugar es pulcro y agradable. Cuenta con terraza hacia la calle de Gante, que es peatonal. El precio no es bajo, pero es razonable.'),
+    array('Caro','La comida es cara para la ración que sirven. No caigas en la tentación de pedir mojitos al 2x1, no están bien preparados, mejor elige las margaritas'),
+    array('Barato','El lugar es de un ambiente muy pesado(peligroso) , la música es muy repetida, el lugar es pequeño y el alcohol es barato o poco diverso'),
+    array('Moderado', 'Una excelente opción dentro del primer cuadro del centro histórico. Buena relación precio-calidad. Es un poco pequeño el lugar y usualmente hay que hacer fila de espera para tener lugar, pero vale la pena.'),
+    array('Caro', 'Muy buen lugar, los precios un poco elevados. También el lugar es pequeño y hay que esperar. El servicio está bien y tiene buen sabor.'),
+    array('Moderado', 'Es un lugar pequeño pero muy acojedor, es bastante limpio y los que te atienden en ese lugar son bastante amables, la comida es muy rica y sus precios no son altos a pesar de que esta en la zona centro'),
+    array('Moderado', 'Buen lugar,excelente vista,buena comida,precios no tan caros,pero sin preguntar ( cosa q ya no está permitida) te clavan la propina obligatoria! Y solo te venden vinos no muy buenos y en eso sí son muuuy caros! Todo lo demás es bueno,ojo estacionamiento cerca solo en bellas artes!"'),
+    array('Muy caro', 'Cierran a las 9pm pero cocina deja de funcionar a las 7:30pm, parece que te están corriendo antes de tiempo y no dejan disfrutar la vista, además de que los precios son excesivos.'),
+    array('Barato', 'Recomiendo los chilaquiles en salsa de cacahuate, el servicio fue bueno. Tiene precios accesibles'),
+    array('Caro', 'O ya mejore mi paladar o decayo el sabor de este menu, siempre me gusto, desde la primera vez pero ahora si le veo varias fallas, te llenas pero le falta sabor a su comida, donde se fue? si subio el precio'),
+    array('Barato', 'Muy buen menú y precios accesibles! Definitivamente volvería!'),
+    array('Caro','Un lugar típico para comer algunas especialidades, un poco caro'),
+    array('Caro','Buen lugar para departir un rato en compañía de los amigos o de la familia, el tratado de los meseros es bueno, los precios algo caros, ambiente familiar, lo recomiendo'),
+    array('Caro', 'La comida no está tan buena, abusan de la grasa. No es malo el lugar pero por los precios podrías esperar más.'),
+    array('Moderado', 'Excelente comida, los postres deliciosos y el café con cardamomo delicioso vale la pena probarlo, el precio es bueno para ser comida \nárabe. El espacio es tranquilo a pesar de estar en medio del centro y la atención es muy buena'),
+    array('Moderado', 'Una gran experiencia!\nUna mezcla de gran atención, platillos diversos con opciones vegetarianas, limpieza en alimentos y en el establecimiento en general. Y los precios son justos para la calidad de los mismos. De mis lugares favoritos. Solo hay que perderle el miedo a la zona que no es del todo bonita por el comercio'),
+    array('Caro', 'Muy bonito lugar. La comida muy buena pero un poco cara. El ambiente agradable'),
+
+);
+
+$tset = new TrainingSet(); // will hold the training documents
+$tok = new WhitespaceTokenizer(); // will split into tokens
+$ff = new DataAsFeatures(); // see features in documentation
+
+// ---------- Training ----------------
+foreach ($training as $d)
+{
+    $tset->addDocument(
+        $d[0], // class
+        new TokensDocument(
+            $tok->tokenize($d[1]) // The actual document
+        )
+    );
+}
+
+$model = new FeatureBasedNB(); // train a Naive Bayes model
+$model->train($ff,$tset);
+
+
+// ---------- Classification ----------------
+$cls = new MultinomialNBClassifier($ff,$model);
+$correct = 0;
+foreach ($testing as $d)
+{
+    // predict if it is spam or ham
+    $prediction = $cls->classify(
+        array('Caro','Muy Caro', 'Moderado', 'Barato'), // all possible classes
+        new TokensDocument(
+            $tok->tokenize($d[1]) // The document
+        )
+    );
+    printf("precio %s\n ",$d[0] );
+    printf("comentario %s\n",$d[1] );
+    printf("!!!Prediccion %s\n", $prediction );
+    if ($prediction==$d[0])
+        $correct ++;
+}
+
+printf("Accuracy: %.2f\n", 100*$correct / count($testing));
+
+
+
+
+    // echo '<pre>' . var_export($tok->tokenize($text), true) . '</pre>';
+    return true;
 });
 
 /**
